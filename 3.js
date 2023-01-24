604455b772d5e2b5cbdeb060307350b2743d505b
underengineering <san4a852b@gmail.com>
2023-01-24 11:41:27 +0300

    Add 3.js

---
+++ 3.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const firstValue = 123;
+const secondValue = 432;
+
+const average = (firstValue + secondValue) / 2;
+
+const fracPart = average % 1;
+console.log("Дробная часть среднего арифметического 1 и 2 числа:", fracPart);
